5e438df38979da4d2202389e23f931d2055aea0a
bmaupin <bmaupin@users.noreply.github.com>
2023-01-18 13:56:26 -0500

    feat: Switch to a reader pattern
    This allows much easier parsing of the binary without having to track specific binary offsets, and it encourages reading properties even if we're not using them, which is a nice way of documenting them if we ever do use them

--- bioshock-infinite/src/Ue3Package.ts
+++ bioshock-infinite/src/Ue3Package.ts
@@ -3,15 +3,18 @@ import invariant from 'tiny-invariant';
 export class Ue3Package {
   private arrayBuffer: ArrayBuffer;
   private nameTable: String[] = [];
+  private reader: UePackageReader;
 
   constructor(arrayBuffer: ArrayBuffer) {
     this.arrayBuffer = arrayBuffer;
+    this.reader = new UePackageReader(this.arrayBuffer);
 
     this.readHeader();
   }
 
+  // http://eliotvu.com/page/unreal-package-file-format
   private readHeader() {
-    const fileSignature = new Uint8Array(this.arrayBuffer, 0, 4);
+    const fileSignature = this.reader.getUint8Array(4);
     invariant(
       Ue3Package.compareUint8Arrays(
         fileSignature,
@@ -20,16 +23,28 @@ export class Ue3Package {
       'Validate file signature'
     );
 
-    const dataView = new DataView(this.arrayBuffer);
-    const folderNameSize = dataView.getUint32(0x10, true);
+    const packageVersion = this.reader.getUint16();
+    // I think UE4 package versions are < 0
+    invariant(packageVersion > 0, 'Package version should be > 0');
 
-    // Skip ahead to
-    const nameCountStartingByte = 0x10 + folderNameSize + 0x04;
-    const nameTableNumEntries = dataView.getUint32(nameCountStartingByte, true);
-    const nameTableStartingByte = dataView.getUint32(
-      nameCountStartingByte + 4,
-      true
-    );
+    const _licenseeVersion = this.reader.getUint16();
+    if (packageVersion >= 249) {
+      const _headerSize = this.reader.getUint32();
+    }
+    // TODO: add version check for this; it's not documented
+    const _unknown = this.reader.getUint32();
+    if (packageVersion >= 269) {
+      const _folderName = this.reader.getString();
+    }
+    const _packageFlags = this.reader.getUint32();
+
+    // // Skip ahead to
+    // const nameCountStartingByte = 0x10 + folderNameSize + 0x04;
+    // const nameTableNumEntries = dataView.getUint32(nameCountStartingByte, true);
+    // const nameTableStartingByte = dataView.getUint32(
+    //   nameCountStartingByte + 4,
+    //   true
+    // );
 
     // this.populateNameTable(nameTableStartingByte, nameTableNumEntries);
   }
@@ -61,16 +76,55 @@ export class Ue3Package {
       //    1. flags (8 bytes)
     }
   }
+}
 
-  private getUeString = (startingByte: number) => {
-    const dataView = new DataView(this.arrayBuffer);
-    const stringLength = dataView.getUint32(startingByte, true);
+class UePackageReader {
+  private arrayBuffer: ArrayBuffer;
+  private byteOffset: number = 0;
+  private dataView: DataView;
+
+  constructor(arrayBuffer: ArrayBuffer) {
+    this.arrayBuffer = arrayBuffer;
+    this.dataView = new DataView(this.arrayBuffer);
+  }
+
+  getUint8Array(length: number): Uint8Array {
+    const uintArray = new Uint8Array(this.arrayBuffer, this.byteOffset, length);
+    this.byteOffset += length;
+
+    return uintArray;
+  }
+
+  getUint16(): number {
+    const uint16 = this.dataView.getUint16(this.byteOffset, true);
+    this.byteOffset += 2;
+    return uint16;
+  }
+
+  getUint32(): number {
+    const uint32 = this.dataView.getUint32(this.byteOffset, true);
+    this.byteOffset += 4;
+    return uint32;
+  }
+
+  // Get a string and update the byte offset. The first 4 bytes are the string length.
+  getString(): string {
+    const stringLength = this.getUint32();
     const uintArray = new Uint8Array(
       this.arrayBuffer,
-      startingByte + 4,
+      this.byteOffset,
       stringLength
     );
+    this.byteOffset += stringLength;
 
     return new TextDecoder('utf8').decode(uintArray).replace(/\0.*$/g, '');
-  };
+  }
+
+  seekToByte(byteOffset: number): void {
+    invariant(
+      byteOffset <= this.arrayBuffer.byteLength,
+      "(seekToByte) offset isn't beyond the end of the file"
+    );
+    this.byteOffset = byteOffset;
+  }
 }
