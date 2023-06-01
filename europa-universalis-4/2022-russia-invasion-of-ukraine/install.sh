d82057aef6ed1a5d4c7e2064f526619ce96358e8
bmaupin <bmaupin@users.noreply.github.com>
2023-06-01 13:50:59 -0400

    feat: First step toward making script idempotent

--- europa-universalis-4/2022-russia-invasion-of-ukraine/install.sh
+++ europa-universalis-4/2022-russia-invasion-of-ukraine/install.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-# Add a couple newlines; use printf instead of echo since sh echo doesn't support echo -e
-printf "\n\n" >> ~/.local/share/Steam/steamapps/workshop/content/236850/217416366/history/countries/"RUS - Russia.txt"
-printf "\n\n" >> ~/.local/share/Steam/steamapps/workshop/content/236850/217416366/history/countries/"UKR - Ukraine.txt"
+marker_text="# New additions start here"
+# Don't quote this or the ~ won't expand
+path_to_mod=~/.local/share/Steam/steamapps/workshop/content/236850/217416366/history/countries
 
-# Add our changes
-cat "RUS - Russia.txt" >> ~/.local/share/Steam/steamapps/workshop/content/236850/217416366/history/countries/"RUS - Russia.txt"
-cat "UKR - Ukraine.txt" >> ~/.local/share/Steam/steamapps/workshop/content/236850/217416366/history/countries/"UKR - Ukraine.txt"
+for country_file in *.txt; do
+    if ! grep -q "${marker_text}" "${path_to_mod}/${country_file}"; then
+        # Add a couple newlines; use printf instead of echo since sh echo doesn't support echo -e
+        printf "\n\n" >> "${path_to_mod}/${country_file}"
+
+        # Add marker text to the file so we can make this script idempotent
+        echo "${marker_text}" >> "${path_to_mod}/${country_file}"
+
+        # Add our changes
+        cat "${country_file}" >> "${path_to_mod}/${country_file}"
+    fi
+done
