ae7fdca3f07e0158c0032d8dfd77fd831db64269
bmaupin <bmaupin@users.noreply.github.com>
2025-03-27 14:02:05 -0400

    feat: Move Civ-related stuff to https://github.com/bmaupin/civ-modding-tools
    ... so we can archive this project

--- alpha-centauri/mod-map-sizes.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cp -an "/home/$USER/.steam/steam/steamapps/common/Sid Meier's Alpha Centauri/alpha.txt" \
-    "/home/$USER/.steam/steam/steamapps/common/Sid Meier's Alpha Centauri/alpha.txt.bak"
-
